35bd43f6b064c12cc5babd659e808410627e5c5a
onlinecodingclub <106621656+onlinecodingclub@users.noreply.github.com>
2022-06-01 09:34:14 +0530

    Add files via upload

---
+++ quiz data/java/js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Which of the following option leads to the portability and security of Java?",
+  answer: "Bytecode is executed by JVM",
+  options: [
+    "Bytecode is executed by JVM",
+    "The applet makes the Java code secure and portable",
+    "Use of exception handling",
+    "Dynamic binding between objects"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which of the following is not a Java features??",
+  answer: "Use of pointers",
+  options: [
+    "Dynamic",
+    "Architecture Neutral",
+    "Use of pointers",
+    "Object-oriented"
+  ]
+},
+  {
+  numb: 3,
+  question: " The \u0021 article referred to as a",
+  answer: "Unicode escape sequence",
+  options: [
+    "Unicode escape sequence",
+    "Octal escape",
+    "Hexadecimal",
+    "Line feed"
+  ]
+},
+  {
+  numb: 4,
+  question: "  _____ is used to find and fix bugs in the Java programs.",
+  answer: "JDB",
+  options: [
+    "JVM",
+    "JVD",
+    "JDB",
+    "JDK"
+  ]
+},
+  {
+  numb: 5,
+  question: " Which of the following is a valid declaration of a char?",
+  answer: " char ch = 'tea';",
+  options: [
+    "char ch = 'tea';",
+    "char ca = 'tea';",
+    "char cr = \u0223;",
+    "char cc = '\itea';"
+  ]
+},
+{
+numb: 6,
+  question: " What does the expression float a = 35 / 0 return?",
+  answer: "Infinity",
+  options: [
+    "0",
+    "Not a Number",
+    "Infinity",
+    "Run time exception"
+  ]
+},
+{
+numb: 7,
+  question: " Which of the following tool is used to generate API documentation in HTML format from doc comments in source code",
+  answer: " Javadoc tool",
+  options: [
+    "javap tool",
+    "javaw command",
+    "Javadoc tool",
+    "javah command"
+  ]
+},
+{
+numb: 8,
+  question: "  Which of the following for loop declaration is not valid?",
+  answer: " for ( int i = 99; i>=0; i / 9)",
+  options: [
+    "for ( int i = 99; i >= 0; i / 9 )",
+    "for ( int i = 7; i <= 77; i += 7 )",
+    "for ( int i = 20; i >= 2; - -i )",
+    "for ( int i = 2; i <= 20; i = 2* i )"
+  ]
+},
+{
+numb: 9,
+  question: " What do you mean by nameless objects?",
+  answer: " An object that has no reference.",
+  options: [
+    "An object created by using the new keyword.",
+    "An object of a superclass created in the subclass",
+    " An object that has no reference",
+    "An object without having any name but having a reference"
+  ]
+},
+{
+numb: 10,
+  question: "An interface with no fields or methods is known as a ______.",
+  answer: "Marker Interface",
+  options: [
+    "Runnable Interface",
+    "Marker Interface",
+    "Abstract Interface",
+    "CharSequence Interface"
+  ]
+},
+
+
+];
